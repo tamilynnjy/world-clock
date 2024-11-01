ea783ecb069939338c5257c2ec8446d3ebc8a09f
tamilyn <tamilynnjy@gmail.com>
2024-11-01 23:11:29 +1100

    added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -41,7 +41,8 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small>
           </div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>`;
 }
 
 updateTime();
